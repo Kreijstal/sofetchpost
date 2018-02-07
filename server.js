09bb3b075aa3943775a559bfdc81da46c8a09399
Glitch (proximate) <none>
2018-02-07 23:16:57 +0000

    🌷⛽️ Checkpoint server.js:39000/476 package.json:39000/21

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
-module.exports = function addCors(req, res) {
-  
+const { parse } = require('url');
+
+const fetch = require('node-fetch');
+
+module.exports = async function addCors(req, res) {
+  const params = parse(req.url, true).query;
+  if (params.secret !== process.env.SECRET) return;
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const data = await fetch(params.url);
+  data.body.pipe(res);
 };
