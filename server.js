1d1053379bcde950b0fffdb0874c77ac30c43c51
Glitch (sofetch) <none>
2018-02-09 05:12:46 +0000

    🔔🛎 Checkpoint server.js:39000/453 package.json:39000/13

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-const { parse } = require('url');
+const fs = require('fs');
 
 const fetch = require('node-fetch');
 
-module.exports = async function addCors(req, res) {
+const historyFilename = '.data/history.txt'
+
+const readHistory = function readHistoryFunc() {
+  fs.readFile(historyFilename, (err, history) => {
+    if (err) 
+  });
+}
+
+module.exports = async function soFetchProxy(req, res) {
   const url = req.url.slice(1);
-  console.log(url);
+  if (url.length === 0) return readHistory();
+  
+  fs.appendFile(historyFilename, `${new Date()} ${url}`);
   const data = await fetch(url);
   res.setHeader('Access-Control-Allow-Origin', '*');
   data.body.pipe(res);
-  console.log(req);
 };
