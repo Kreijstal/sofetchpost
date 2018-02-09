048e48a1ea168e3e17003ed326e249be7e5842b2
Glitch (sofetch) <none>
2018-02-09 01:50:50 +0000

    🐜🌹 Checkpoint .env:39000/6 server.js:39000/132

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const { parse } = require('url');
 const fetch = require('node-fetch');
 
 module.exports = async function addCors(req, res) {
-  const params = parse(req.url, true).query;
+  const url = req.url.slice(1);
+  console.log(url);
   res.setHeader('Access-Control-Allow-Origin', '*');
-  const data = await fetch(params.url);
+  const data = await fetch(url);
   data.body.pipe(res);
   console.log('done');
 };
