89a952d5a7bb4ab3c74b1e5d53e387f072c4fe5f
Glitch (foggy-knave) <none>
2021-01-11 08:42:07 +0000

    🐁🍅 Checkpoint ./server.js:17747925/2249

--- server.js
+++ server.js
@@ -34,17 +34,59 @@ module.exports = async function soFetchProxy(req, res) {
     if(uri(url).protocol()=="ftp"){
       var c = new ftp();
   c.on('ready', function() {
-    /*c.list(function(err, list) {
+    var p=decodeURI(uri(url).path());
+    if(p[p.length-1]=="/"){                
+    c.list(p,function(err, list) {
       if (err) throw err;
       console.dir(list);
+      `<!DOCTYPE html>
+<html>
+<head>
+	<title>Index of ${p}</title>
+	<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+	<meta content="width=640, maximum-scale=4, initial-scale=1.0" name="viewport">
+	<meta content="yes" name="apple-mobile-web-app-capable">
+</head>
+<body>
+	<h1>Index of ${p}</h1>
+	<table>
+		<tr>
+			<th valign="top"><img alt="[ICO]" src="https://sofetchpost.glitch.me/http://www.xray.mpe.mpg.de/icons/apache/blank.gif"></th>
+			<th>Name</th>
+			<th>Last modified</th>
+			<th>Size</th>
+			<th>Description</th>
+		</tr>
+		<tr>
+			<th colspan="5">
+				<hr>
+			</th>
+		</tr>
+		<tr>
+			<td valign="top"><img alt="[PARENTDIR]" src="https://sofetchpost.glitch.me/http://www.xray.mpe.mpg.de/icons/apache/back.gif"></td>
+			<td><a href="/">Parent Directory</a></td>
+			<td>&nbsp;</td>
+			<td align="right">-</td>
+			<td>&nbsp;</td>
+		</tr>`+
+      list.map(a=>`		<tr>
+			<td valign="top"><img alt="${a.type=="d"?"[DIR]":"[TXT]"}" src="https://sofetchpost.glitch.me/http://www.xray.mpe.mpg.de/icons/apache/${a.type=="d"?"folder.gif":"text.gif"}"></td>
+			<td>
+				<a href="${p+encodeURI(a.name)}">${a.name}</a>
+			</td>
+			<td align="right">${a.date}</td>
+			<td align="right">${a.size}</td>
+			<td>&nbsp;</td>
+		</tr>`).join('\n')
       c.end();
-    });*/
-    
-     c.get(decodeURI(uri(url).path()), function(err, stream) {
+    });
+    }else{
+     c.get(p, function(err, stream) {
       if (err){console.log(decodeURI(uri(url).path()),"did you really just error on me?"); throw err};
       stream.once('close', function() { c.end(); });
       stream.pipe(res);
     });
+    }
   });
   
   c.connect(uri(url).parts);
